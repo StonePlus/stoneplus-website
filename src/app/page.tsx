69fefcd5e9c777feb5f914a2bdf154089c220d60
Amarilio de Oliveira <amarilioalencar@gmail.com>
2023-07-25 23:41:57 -0300

    Add componenent for Testimony section.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Hero } from "@/components/layout/Hero";
 import { Mission } from "@/components/layout/Mission";
+import { Testimony } from "@/components/layout/Testimony";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <Mission />
+      <Testimony />
     </main>
   );
 }
