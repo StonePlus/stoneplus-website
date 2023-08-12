1e201cc2840237e948c447b0020e9d7d746a7943
Amarilio de Oliveira <amarilioalencar@gmail.com>
2023-08-11 21:48:54 -0300

    Fix type inconsistencies in Link and DynamicHeroIcons components.

--- src/components/ui/DynamicHeroIcons.tsx
+++ src/components/ui/DynamicHeroIcons.tsx
@@ -9,16 +9,11 @@ interface DynamicHeroIconProps {
 /**
  * Component that displays a dynamic icon from HeroIcons.
  * @param {string} icon - Icon name to be displayed. Refer to the complete list of available icons at {@link https://unpkg.com/browse/@heroicons/react@2.0.18/24/outline/}.
- * @returns {JSX.Element} The rendered HeroIcon.
+ * @returns {React.FC} The rendered HeroIcon.
  */
 
 export const DynamicHeroIcon: React.FC<DynamicHeroIconProps> = ({ icon }) => {
   const SelectedIcon = HeroIconsOutline[icon];
-
-  if (!SelectedIcon) {
-    // Handle invalid icon.
-    return null;
-  }
 
   return (
     <SelectedIcon
--- src/components/ui/Links.tsx
+++ src/components/ui/Links.tsx
@@ -1,19 +1,22 @@
 import Link from "next/link";
-import { DynamicHeroIcon , IconName } from "./DynamicHeroIcons";
+import { Url } from "next/dist/shared/lib/router/router";
+
+import { DynamicHeroIcon, IconName } from "./DynamicHeroIcons";
 
 interface LinksProps {
-    linkType: IconName;
-    linkLabel: string;
-  }
+  href: Url;
+  linkType?: IconName;
+  label: string;
+}
 
-export const Links: React.FC<LinksProps> = ({ linkLabel , linkType }) => {
+export const Links: React.FC<LinksProps> = ({ label, linkType, href }) => {
   return (
     <Link
       className="flex items-center gap-2 whitespace-nowrap hover:text-gunmetal-50 stroke-gunmetal-300 hover:stroke-gunmetal-50 "
-      href="#"
+      href={href}
     >
-      {linkLabel}
-      <DynamicHeroIcon icon={linkType} />
+      {label}
+      {linkType && <DynamicHeroIcon icon={linkType} />}
     </Link>
   );
 };
