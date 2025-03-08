154edfba9e519bfcb9eb9fd17342a0a9efcf4580
Amarilio de Oliveira <amarilioalencar@gmail.com>
2025-03-08 01:44:07 +0000

    Fix to make the WhatsApp Toast disappear at the final of the page.

--- src/components/ui/WhatsAppToast.tsx
+++ src/components/ui/WhatsAppToast.tsx
@@ -11,11 +11,14 @@ import WhatsappLogo from "@img/icons/whatsapp-digital-white.png";
 const WhatsAppToast: React.FC = () => {
   const scrollPosition = useScrollPosition();
 
+  const isAtBottom =
+    scrollPosition + window.innerHeight >= document.body.offsetHeight;
+
   return (
     <div className="toast toast-end">
       <Link
         href="https://wa.me/558532602501"
-        className={`${scrollPosition > 800 ? "inline-flex" : "hidden"} btn btn-circle btn-success btn-lg z-50 shadow duration-200 ease-in-out`}
+        className={`${scrollPosition > 800 && !isAtBottom ? "inline-flex" : "hidden"} btn btn-circle btn-success btn-lg z-50 shadow duration-200 ease-in-out`}
         target="_blank"
       >
         <Image src={WhatsappLogo} width={32} height={32} alt="WhatsApp Logo" />
